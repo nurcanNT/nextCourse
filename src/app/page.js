1261492b1ecfce34f2e6dc08457ae4229e0427db
nurcanNT <nrcntpkra@gmail.com>
2024-09-10 18:24:16 +0300

    feat: first commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        
         <ol>
           <li>
             Get started by editing <code>src/app/page.js</code>.
