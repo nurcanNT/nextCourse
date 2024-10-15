5acd054287ed4018d72f959ff089a1dcc09d4c81
nurcanNT <nrcntpkra@gmail.com>
2024-10-15 23:22:03 +0300

    feat: fix

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../styles/Theme';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
